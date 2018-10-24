83fef2844809b4ec561ce5c9f1b4bc69fc5dc39a
Pavan Boorla <pushkarapavan@gmail.com>
2018-10-24 14:24:05 +0530

    Fixed an error in cout in 6.cpp

--- exercises/CH1/6.cpp
+++ exercises/CH1/6.cpp
@@ -9,7 +9,7 @@ std::cout << "Welcome .. Mr." + name1 <<std::endl;
 std::string name2;
 std::cout << "Please enter your name"<<std::endl;
 std::cin >> name2;
-std::cout << "Welcome .. Mr." + name1 <<std::endl;
+std::cout << "Welcome .. Mr." + name2 <<std::endl;
 
 
 return 0;
